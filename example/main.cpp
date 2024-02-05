780e8e97aca5325e8950add9872640dc3adb975b
Marian Klymov <nekto1989@gmail.com>
2024-02-05 17:30:00 +0200

    Replace fmt with libpng

--- example/main.cpp
+++ example/main.cpp
@@ -1,6 +1,16 @@
-#include "fmt/color.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "png.h"
+
 
 int main() {
-    fmt::print(fg(fmt::terminal_color::cyan), "Hello fmt {}!\n", FMT_VERSION );
-    return 0;
+    png_structp png_ptr;
+    png_infop info_ptr;
+
+    fprintf(stderr, "   Compiled with libpng %s; using libpng %s.\n", PNG_LIBPNG_VER_STRING, png_libpng_ver);
+    png_ptr = png_create_read_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+    info_ptr = png_create_info_struct(png_ptr);
+
+    return EXIT_SUCCESS;
 }
